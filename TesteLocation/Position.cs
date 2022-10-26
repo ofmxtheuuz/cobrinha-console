6e25d4bbaa6a4596738cd8ca658b4aff93cdb2c1
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-10-26 17:49:34 -0300

    Add files via upload

---
+++ TesteLocation/Position.cs
@@ -0,0 +1,12 @@
+﻿namespace TesteLocation;
+
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString()
+    {
+        return "{ X: " + this.X + ", Y: " + this.Y + " }";
+    }
+}
---
+++ TesteLocation/Program.cs
@@ -0,0 +1,85 @@
+﻿using TesteLocation;
+
+int score = 0, foodX = 0, foodY = 0;
+
+Position[] positions = new Position[1];
+positions[0] = new Position() { X = 1, Y = 1};
+
+Start();
+SetFood();
+Info();
+
+Console.WriteLine();
+
+while (true)
+{
+    var key =  Console.ReadKey();
+    Console.Clear();
+    switch (key.Key)
+    {
+        case ConsoleKey.W:
+            positions[0].Y++;
+            if (positions[0].Y == foodY && positions[0].X == foodX)
+            {
+                score++;
+                SetFood();
+            }
+            break;
+        case ConsoleKey.S:
+            positions[0].Y--;
+            if (positions[0].Y == foodY && positions[0].X == foodX)
+            {
+                score++;
+                SetFood();
+            }
+            break;
+        case ConsoleKey.D:
+            positions[0].X++;
+            if (positions[0].Y == foodY && positions[0].X == foodX)
+            {
+                score++;
+                SetFood();
+            }
+            break;
+        case ConsoleKey.A:
+            positions[0].X--;
+            if (positions[0].Y == foodY && positions[0].X == foodX)
+            {
+                score++;
+                SetFood();
+            }
+            break;
+    }
+
+    if (score != 5)
+    {
+        Info();
+    }
+    else
+    {
+        Console.WriteLine("You won!");
+    }
+}
+
+void Start()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Title = "Jogo da cobrinha";
+    Console.WriteLine("Jogo da cobrinha (no console)");
+    Console.Beep();
+}
+
+void SetFood()
+{
+    foodX = new Random().Next(1, 20);
+    foodY = new Random().Next(1, 20);
+}
+
+void Info()
+{
+    Console.WriteLine($"Food X: {foodX}, Food Y: {foodY}");
+    Console.WriteLine($"Score: {score}");
+    Console.WriteLine(positions[0]);
+    Console.WriteLine();
+}
